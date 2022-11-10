cca35cddf6d1c0ed46a9fddf766bb9e2dd626f93
hasansikdar <hasansikdar973@gmail.com>
2022-11-10 19:17:08 +0600

    video section added

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -66,6 +66,9 @@ const Home = () => {
                 </div>
             </div>
 
+            <div className="hero">
+                <iframe width="100%" height="500" src="https://www.youtube.com/embed/AN524IrRcW0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
 
         </div>
     );
